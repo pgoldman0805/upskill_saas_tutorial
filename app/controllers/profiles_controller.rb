135d18cbc21f6a71e433d4fc703d46af3c767ada
Paul Goldman <paulgoldmanmusic@gmail.com>
2016-12-30 18:05:55 +0000

    Added User Profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,9 @@ class ProfilesController < ApplicationController
   # GET request to /users/:user_id/profile/new
   def new
     # Render a blank profile details form
+    @profile = Profile.new
+  end
+  def create
+    
   end
 end
